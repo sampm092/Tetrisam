1120270a9bcdd1a0b28c2d8144cd5ac0fcbe6ca5
SamPM092 <sammich4294@gmail.com>
2025-08-11 15:12:04 +0700

    just 2 block running (2)

--- Assets/randomscript.cs
+++ Assets/randomscript.cs
@@ -1,24 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class randomscript : MonoBehaviour
 {
     public Rigidbody2D MyRigid;
     public float scrollSpeed1;
-    public float scrollSpeed2;
-    public float resetPositionX = -8; // Where to reset the tile
-    public float startPositionX = 5; // Where to move it back to
-    private float BgWidth;
-    // Start is called before the first frame update
+    public float changeInterval = 1f; // seconds between randomizations
+
+    private float timer;
 
     void Start()
     {
-        scrollSpeed1 = Random.Range(0.5f, 3f);
-        scrollSpeed2 = Random.Range(0.5f, 3f);
+        RandomizeSpeeds();
     }
+
     void Update()
     {
+        // Move the objects
         MyRigid.transform.position += Vector3.right * scrollSpeed1 * Time.deltaTime;
+
+        // Update timer
+        timer += Time.deltaTime;
+        if (timer >= changeInterval)
+        {
+            RandomizeSpeeds();
+            timer = 0f;
+        }
+    }
+
+    void RandomizeSpeeds()
+    {
+        scrollSpeed1 = Random.Range(0.5f, 3f);
     }
 }
