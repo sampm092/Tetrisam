a89f62fdf0a83c0443e716c314f55cd6471cab04
SamPM092 <sammich4294@gmail.com>
2025-08-22 13:55:15 +0700

    refactor: making scene changing scripts into one

--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GOScript : MonoBehaviour
+public class SceneScript : MonoBehaviour
 {
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
     public void RetryGame()
     {
         SceneManager.LoadScene("Level");
