bd17b910dc92999d653e5bb228bbbd2d68f11333
SamPM092 <sammich4294@gmail.com>
2025-09-16 13:17:41 +0700

    fix: bgm intersection between menu and stage

--- Assets/Scripts/BGMScript.cs
+++ Assets/Scripts/BGMScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BGMScript : MonoBehaviour
 {
     private static BGMScript instance;
+    private AudioSource audioSource;
 
     void Awake()
     // Awake() is called before Start(), as soon as the object is created.
@@ -13,10 +15,25 @@ public class BGMScript : MonoBehaviour
         {
             instance = this; //assign this crip
             DontDestroyOnLoad(gameObject); // keep this across scenes even after loading other gameobjects
+            audioSource = GetComponent<AudioSource>();
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
             Destroy(gameObject); // destroy other gameObject (another bgm) if theres another instance
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Level")
+        {
+            audioSource.Stop(); // no music in Scene C
+        }
+        else
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play(); // resume for A & B
         }
     }
 }
