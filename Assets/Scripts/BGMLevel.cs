8f0febd60668a001db37f64c851e6d796a3aa129
SamPM092 <sammich4294@gmail.com>
2025-09-30 15:22:38 +0700

    feat: implementing settings to level scene done

---
+++ Assets/Scripts/BGMLevel.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class BGMLevel : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    [SerializeField]
+    private Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            musicSlidePos();
+        }
+        else
+        {
+            setMusicVolume();
+        }
+    }
+
+    // Update is called once per frame
+    public void setMusicVolume()
+    {
+        float volume = volumeSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    void musicSlidePos()
+    {
+        if (volumeSlider != null)
+        {
+            // Make sure the slider starts at the current volume
+            volumeSlider.value = PlayerPrefs.GetFloat("musicVolume", 1f); // or load from PlayerPrefs / default
+
+            // Subscribe to value change
+            volumeSlider.onValueChanged.AddListener(value =>
+            {
+                PlayerPrefs.SetFloat("musicVolume", value);
+                setMusicVolume(); // set volume value based on slider
+            });
+        }
+    }
+}
